fb55a7e64cc231bd94840735770b96284057b39f
lps08 <lucianolps08@gmail.com>
2022-05-13 15:00:10 -0300

    fix the connection with the database

--- controllers/database/database.php
+++ controllers/database/database.php
@@ -1,20 +1,15 @@
 <?php
 
 class DBClass {
-
-    private static $host = $_ENV["HOST"];
-    private static $username = $_ENV["DB_USER"];
-    private static $password = $_ENV["DB_PASSWD"]; 
-    private static $database = $_ENV["DB_NAME"];
    
     public static function connect(){
        $connection = null;
-   
+          
        try{
-           $connection = new PDO("mysql:host=" . self::$host . ";dbname=" . self::$database, self::$username, self::$password);
+           $connection = new PDO("mysql:host=" . $_ENV["DB_LINK_NAME"] . ";dbname=" . $_ENV["DB_NAME"], $_ENV["DB_USER"], $_ENV["DB_PASSWD"]);
            $connection->exec("set names utf8");
        }catch(PDOException $exception){
-           echo "Error: " . $exception->getMessage();
+           echo "Error mt error: " . $exception->getMessage();
        }
        return $connection;
     }
